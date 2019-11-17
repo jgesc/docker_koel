fdb8b4d4ac05ca6c2537c94697d77990c7b180ff
jgesc <37676259+jgesc@users.noreply.github.com>
2019-11-17 12:53:17 +0100

    Update generate-certificate.sh

--- generate-certificate.sh
+++ generate-certificate.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+if [!-d ./persist]; then
+    mkdir ./persist;
+	mkdir ./persist/ssl;
+	mkdir ./persist/ssl/private;
+	mkdir ./persist/ssl/certs;
+fi;
+
 cd persist
 openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout ssl/private/koel.key -out ssl/certs/koel.crt
